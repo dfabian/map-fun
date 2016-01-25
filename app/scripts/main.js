a2cbc92a91b8f97e140010e8e076bbd7c2a9613f
darrick fabian <dfabian@solutionset.com>
2016-01-25 08:38:02 -0800

    commit it x3

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,42 @@
-// console.log('\'Allo \'Allo!');
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+var elMap = document.getElementById('loc');                 // HTML element
+var msg = 'Sorry, we were unable to get your location.';    // No location msg
+var long = 0;
+var lat = 0;
+
+
+if (Modernizr.geolocation) {                                // Is geo supported
+  navigator.geolocation.getCurrentPosition(success, fail);  // Ask for location
+  elMap.textContent = 'Checking location...';               // Say checking...
+} else {                                                    // Not supported
+  elMap.textContent = msg;                                  // Add manual entry
+}
+
+function success(position) {                                // Got location
+  msg = '<h3>Longitude:<br>';                               // Create message
+  msg += position.coords.longitude + '</h3>';               // Add longitude
+  msg += '<h3>Latitude:<br>';                               // Create message
+  msg += position.coords.latitude + '</h3>';                // Add latitude
+  elMap.innerHTML = msg;
+  long = position.coords.longitude;
+  lat = position.coords.latitude;                                    // Show location
+}
+
+function fail(msg) {                                        // Not got location
+  elMap.textContent = msg;                                  // Show text input
+  console.log(msg.code);                                    // Log the error
+}
+
+console.log('lat', lat)
+
+
+
 function init() {
   var mapOptions = {                                 // Set up the map options
-    center: new google.maps.LatLng(37.7833,-122.4167),
+    center: new google.maps.LatLng(lat,long),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     zoom: 13
   };
@@ -11,7 +46,7 @@ function init() {
 
 function loadScript() {
   var script = document.createElement('script');     // Create <script> element
-  script.src = 'http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
+  script.src = 'http://maps.googleapis.com/maps/api/js?callback=init';
   document.body.appendChild(script);                 // Add element to page
 }
 
